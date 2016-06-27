338d905e9dc7b52b81d74d29ff45420c66be6469
wangrenlang <1171726498@qq.com>
2016-06-27 07:29:46 +0000

    Add files via upload

---
+++ sjb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main()
+{
+	int a,b;
+	printf("0表示石头，1表示剪刀，2表示布\n");
+	printf("请选择输入0/1/2:\n");
+	scanf("%d\n",n);
+	if(n!=0&&n!=1&&n!=2)
+		break;
+	b=rand()%3;
+
+}
